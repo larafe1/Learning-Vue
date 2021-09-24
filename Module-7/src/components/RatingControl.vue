67bf09a94b7969ad4cc097922de2fed1343b7d91
Felipe Lara <68015575+felpshn@users.noreply.github.com>
2021-09-24 10:19:20 -0300

    Update: Module 7 finished

---
+++ Module-7/src/components/RatingControl.vue
@@ -0,0 +1,60 @@
+<template>
+  <ul>
+    <li :class="{ active: modelValue === 'poor' }">
+      <button type="button" @click="handleActivate('poor')">Poor</button>
+    </li>
+    <li :class="{ active: modelValue === 'average' }">
+      <button type="button" @click="handleActivate('average')">Average</button>
+    </li>
+    <li :class="{ active: modelValue === 'great' }">
+      <button type="button" @click="handleActivate('great')">Great</button>
+    </li>
+  </ul>
+</template>
+
+<script>
+const RatingControl = {
+  props: ['modelValue'],
+  emits: ['update:modelValue'],
+  methods: {
+    handleActivate(option) {
+      this.$emit('update:modelValue', option);
+    }
+  }
+};
+
+export default RatingControl;
+</script>
+
+<style scoped>
+.active {
+  border-color: #a00078;
+}
+
+.active button {
+  color: #a00078;
+}
+
+ul {
+  list-style: none;
+  margin: 0.5rem 0;
+  padding: 0;
+  display: flex;
+}
+
+li {
+  margin: 0 1rem;
+  border: 1px solid #ccc;
+  padding: 1rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+button {
+  font: inherit;
+  border: none;
+  background: transparent;
+  cursor: pointer;
+}
+</style>
